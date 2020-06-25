3cc4034f7c15222866a5aad35d02c5888782eab6
roman9891 <roman9891@gmail.com>
2020-06-25 17:37:28 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,31 @@
 class Customer
-  
+    attr_accessor :name, :age
+    
+    @@all = []
+    
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meals|
+            meals.customer == self
+        end
+    end
+
+    def waiters
+        self.meals.map do |meals_with_this_customer|
+            meals_with_this_customer.waiter
+        end
+    end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,31 @@
 class Waiter
+    attr_accessor :name, :age
+    @@all = []
+    
+    def initialize(name, years)
+        @name = name
+        @years = years
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meals|
+            meals.waiter == self
+        end
+    end
+
+    def best_tipper
+        sorted_tips_array = Meal.all.sort_by do |meals|
+            meals.tip
+        end
+        sorted_tips_array.last.customer
+    end
 end
